d4810bca3518f36b65a691f4656ea83043aff89d
Chris Speciale <christopher.gaf@gmail.com>
2024-05-29 06:31:00 -0400

    Implement OAuth

---
+++ src/crossbyte/auth/OAuthConfig.hx
@@ -0,0 +1,35 @@
+package crossbyte.auth;
+
+/**
+ * ...
+ * @author Christopher Speciale
+ */
+package crossbyte.auth;
+
+/**
+ * OAuth configuration class.
+ */
+class OAuthConfig {
+    public var clientId: String;
+    public var clientSecret: String;
+    public var authorizeUrl: String;
+    public var tokenUrl: String;
+    public var redirectUri: String;
+
+    /**
+     * Constructs a new OAuthConfig instance.
+     *
+     * @param clientId The client ID.
+     * @param clientSecret The client secret.
+     * @param authorizeUrl The authorization URL.
+     * @param tokenUrl The token URL.
+     * @param redirectUri The redirect URI.
+     */
+    public function new(clientId: String, clientSecret: String, authorizeUrl: String, tokenUrl: String, redirectUri: String) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.authorizeUrl = authorizeUrl;
+        this.tokenUrl = tokenUrl;
+        this.redirectUri = redirectUri;
+    }
+}
